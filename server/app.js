438fc7a1b8a08956363284ff7081f21f7eb38f22
anantharkrishnan <ananthakrishnan1102@gmail.com>
2025-12-27 02:54:39 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "movie-ticket-bookingfrontend.vercel.app",
   credentials: true
 }));
 
