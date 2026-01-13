63ee4e6ac5e8b7551f5111fbf0eb5342cb046d89
anantharkrishnan <ananthakrishnan1102@gmail.com>
2026-01-13 21:41:39 +0530

    updated cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -82,7 +82,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "None",
       maxAge: 60 * 60 * 1000
     });
 
